360e0657f8ae9565228e5edf700507d488f624aa
Manuel Rico <manuelricolopez@liceolapaz.net>
2025-02-17 13:54:48 +0100

    Métodos successful y unsuccessful authentication casi acabados

--- src/main/java/edu/rico/security/auth/filters/JwtAuthenticationFilter.java
+++ src/main/java/edu/rico/security/auth/filters/JwtAuthenticationFilter.java
@@ -1,6 +1,9 @@
 package edu.rico.security.auth.filters;
 
 import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -39,14 +42,30 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         }
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
         return authenticationManager.authenticate(authToken);
-        
     }
 
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, Authentication authResult) throws IOException {
-        
+            String username = ((org.springframework.security.core.userdetails.User) authResult.getPrincipal()).getUsername();
+            String originalInput = "PALABRA_SECRETA." + username;
+            String token = Base64.getEncoder().encodeToString(originalInput.getBytes());
+
+            response.addHeader("Authorization", "Bearer " + token);
+            Map<String, Object> body = new HashMap<>();
+            body.put("token", token);
+            body.put("mensaje", String.format("Hola %s, has iniciado sesión con éxito", username));
+            body.put("username", username);
+            response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+            response.setStatus(200);
+            response.setContentType("application/json");
+            
     }
 
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException {
-        
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Error en el login!!!!! username o password incorrecto");
+        body.put("error", failed.getMessage());
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(401);
+        response.setContentType("application/json");
     }
 }
